af42fd940e752c05c09e42313be57dbd1b0d120e
julie crosby <crosby823@gmail.com>
2012-05-09 22:34:07 -0400

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,21 +6,36 @@ describe "StaticPages" do
     it "should have the content 'Sample App" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text =>'Sample App')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+            :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
   describe "Help page" do
     it "should have the content 'Help'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text =>'Help')
     end
+      it "should have the right title" do
+        visit '/static_pages/help'
+        page.should have_selector('title',
+              :text => "Ruby on Rails Tutorial Sample App | Help")
+      end
   end
   describe "About page" do
     it "should have the content 'About'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/about'
-      page.should have_content('About')
+      page.should have_selector('h1', :text =>'About')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+            :text => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 end
